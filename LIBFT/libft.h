db54fc04bb90b34f9377f6f1dc242c2a6640fbc7
MOHAMMED TAHALLA <111022553+SimoAs@users.noreply.github.com>
2024-09-13 21:25:34 +0100

    push

--- LIBFT/libft.h
+++ LIBFT/libft.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 #include <stdlib.h>
+#include <unistd.h>
 void    *ft_memset(void *b, int c, size_t len);
 void    ft_bzero(void *s, size_t n);
 void    *ft_memcpy(void *dst, const void *src, size_t n);
@@ -33,5 +34,6 @@ char *ft_strtrim(char const *s1, char const *set);
 char **ft_split(char const *s, char c);
 char *ft_itoa(int n);
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void ft_putchar_fd(char c, int fd);
 #endif
 
--- LIBFT/main.c
+++ LIBFT/main.c
@@ -1,28 +1,6 @@
 #include "libft.h" // Assurez-vous que ce fichier d'en-tête contient les prototypes des fonctions que vous utilisez.
 #include <stdio.h>
-char to_uppercase( unsigned int index, char c) 
+int main()
 {
-
-    if (c >= 'a' && c <= 'z') {
-	    index++;
-        return c - 'a' + 'A'; // Convert lowercase to uppercase
-    }
-    return c;
-}
-int main(void) {
-    char *original = "hello world";
-    char *transformed;
-
-    // Apply the ft_strmapi function with the to_uppercase function
-    transformed = ft_strmapi(original, to_uppercase);
-
-    if (transformed) {
-        printf("Original string: %s\n", original);
-        printf("Transformed string: %s\n", transformed);
-        free(transformed); // Don't forget to free the allocated memory
-    } else {
-        printf("Memory allocation failed\n");
-    }
-
-    return 0;
+	 ft_putchar_fd('C', 1);
 }
